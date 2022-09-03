bae187025d2002b19bc6fc6ad8c52c275812b3ce
Nancyjha <nancyvats00@gmail.com>
2022-09-03 22:24:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+ import { ToastContainer } from "react-toastify";
+import { useState } from "react";
+import { useWeb3Transfer } from "react-moralis";
+import { errorToast, successToast } from "./toastbar";
+import "react-toastify/dist/ReactToastify.css";
+import { Button, Input } from '@mui/material';
 
 function App() {
+  const [contractAddress, setContractAddress] = useState("");
+  const [receiverAddress, setReceiverAddress] = useState("");
+  const { fetch, _, isFetching } = useWeb3Transfer({
+    type: "erc1155",
+    receiver: receiverAddress,
+    contractAddress: contractAddress,
+    tokenId: 1,
+  });
+
+  const handleTransfer = async () => {
+    try {
+      await fetch();
+      successToast("Transfer Successful");
+    } catch (error) {
+      errorToast("Transfer Failed");
+    }
+  };
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Input value={contractAddress} onChange={(e) => setContractAddress(e.target.value)} placeholder="Contract Address" />
+      <br />
+      <Input value={receiverAddress} onChange={(e) => setReceiverAddress(e.target.value)} placeholder="Reciever Address" />
+      <br />
+      <Button variant="contained" onClick={() => handleTransfer()} disabled={isFetching}>Transfer</Button>
     </div>
   );
 }
---
+++ src/toastbar.js
@@ -0,0 +1,28 @@
+import { toast } from "react-toastify";
+
+
+
+const successToast = (message) => {
+    toast.success(message, {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+}
+const errorToast = (message) => {
+    toast.error(message, {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+}
+
+export { successToast, errorToast }
